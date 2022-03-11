1db7a8326fd0d932851b0ed6a843806a74bf9b8e
Mohammadreza Asgari <mr.asgari8015@gmail.com>
2022-03-11 13:07:28 +0330

    Added password hashing for user registration

--- src/routers/task.js
+++ src/routers/task.js
@@ -48,14 +48,16 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findById(req.params.id)
+    updates.forEach(update => task[update] = req.body[update])
+    await task.save();
+
     if (!task) {
       return res.status(404).send();
     }
+    
     res.send(task);
+
   } catch (err) {
     res.status(400).send(err);
   }
