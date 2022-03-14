033d1ba2acfc2579014a2ab07931cb7b1c9b7f63
Mohammadreza Asgari <mr.asgari8015@gmail.com>
2022-03-14 13:24:46 +0330

    Added pagination and sort options for getting data back

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,8 +17,30 @@ router.post("/tasks", async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
+
   try {
-    await req.user.populate("tasks").execPopulate();
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (err) {
     res.status(500).send(err);
@@ -69,7 +91,10 @@ router.patch("/tasks/:id", auth, async (req, res) => {
 
 router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
