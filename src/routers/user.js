c86f5d4c5bd1046942fcd08f5e8a4c7b47122ca5
Mohammadreza Asgari <mr.asgari8015@gmail.com>
2022-03-14 18:04:19 +0330

    Added user profile upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require("sharp");
+
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("File must be an image"));
+    }
+    cb(undefined, true);
+  },
+});
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -50,8 +64,39 @@ router.post("/users/logoutAll", auth, async (req, res) => {
   }
 });
 
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
 });
 
 router.patch("/users/me", auth, async (req, res) => {
@@ -83,6 +128,16 @@ router.delete("/users/me", auth, async (req, res) => {
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
+  }
+});
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send;
   }
 });
 
